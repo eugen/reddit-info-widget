3688931d2fd9898650e087b2674be981ec6cfcb9
uaca-uaca <eugen@syntactic.org>
2010-10-10 23:20:33 +0300

    widget displays the upvote and comment counts

---
+++ reddit-info-widget.js
@@ -0,0 +1,28 @@
+//NOTE: this requires jQuery
+function loadRedditInfo(template) {
+  function processResponse(json) {
+    if(json.kind != "Listing" ||
+       json.data.children.size == 0) {
+      // don't show anything if this url hasn't been submitted to reddit
+      return;
+    }
+
+    // we only care about the first entry
+    var entry = json.data.children[0].data;
+
+    template = $(template);
+
+    template.html(template.html().
+      replace("{upvotes}", entry.ups).
+      replace("{score}", entry.score).
+      replace("{comment_link}", "http://reddit.com" + entry.permalink).
+      replace("{comment_count}", entry.num_comments));
+    template.show();
+  };
+  $.ajax({ url: "http://www.reddit.com/api/info.json?url=" + encodeURI(location.href),
+           jsonp: "jsonp",
+           dataType: "jsonp",
+           // the try/catch is ugly, but necessary in case reddit changes their API
+           success: function(json) { try { processResponse(json); } catch(e) { }; }
+  });
+}
