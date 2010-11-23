75fcd4c6a933c371a067a80605b3371556aa59b7
Eugen Anghel <eugen@syntactic.org>
2010-11-23 22:51:29 +0200

    added another function that can fetch the top posts for a domain

--- reddit-info-widget.js
+++ reddit-info-widget.js
@@ -1,12 +1,12 @@
 //NOTE: this requires jQuery
-function loadRedditInfo(target) {
+reddit = (function() {
   function replaceFields(html, data) {
     for(var prop in data) {
       html = html.replace("$" + prop, data[prop]);
     }
     return html;
   };
-  function processResponse(json) {
+  function renderWidgetResponse(json, target) {
     if(json.kind != "Listing" ||
        json.data.children.size == 0) {
       // don't show anything if this url hasn't been submitted to reddit
@@ -19,13 +19,41 @@ function loadRedditInfo(target) {
     target.html(replaceFields(target.html(), entry));
     target.show();
   };
-  $.ajax({ url: "http://www.reddit.com/api/info.json?url=" + encodeURI(location.href),
-           jsonp: "jsonp",
-           dataType: "jsonp",
-           // the try/catch is ugly, but necessary in case reddit changes their API
-           success: function(json) {
-             try { processResponse(json); }
-             catch(e) { };
-           }
-  });
-}
+
+  function renderTopLinksResponse(json, target) {
+    target = $(target);
+    var template = target.html();
+    var children = [];
+    $.each(json.data.children, function(idx, entry) {
+      //hack so that we have an index
+      entry.data.num = idx;
+      children.push(replaceFields(template, entry.data));
+    });
+    target.html(children.join(""));
+  };
+
+  return {
+    info: function(target) {
+      $.ajax({ url: "http://www.reddit.com/api/info.json?url=" + encodeURI(location.href),
+        jsonp: "jsonp",
+        dataType: "jsonp",
+        // the try/catch is ugly, but necessary in case reddit changes their API
+        success: function(json) {
+          try { renderWidgetResponse(json, target); }
+          catch(e) { };
+        }
+      });
+    },
+    topPostsForDomain: function(domain, target) {
+      $.ajax({ url: "http://www.reddit.com/domain/"+encodeURI(domain)+"/top.json?t=week",
+        jsonp: "jsonp",
+        dataType: "jsonp",
+        // the try/catch is ugly, but necessary in case reddit changes their API or reddit is down
+        success: function(json) {
+          try { renderTopLinksResponse(json, target); }
+          catch(e) { };
+        }
+      });
+    }
+  }
+})();
